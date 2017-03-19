57dd2abf505d092bbde92c0d5f0a14d852bcbbfc
Erin Kinnen <erin.kinnen@outlook.com>
2017-03-18 20:35:57 -0500

    moves object to server

--- server/app.js
+++ server/app.js
@@ -5,6 +5,26 @@ var bodyParser = require('body-parser');
 var path = require('path');
 
 app.use(express.static('server/public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post("/values", function(req, res){
+  var values= req.body;
+  console.log(values);
+  var answer = {};
+  answer.result = (parseInt(values.firstValue) + parseInt(values.secondValue));
+  console.log(answer);
+
+  res.sendStatus(200);
+  // result.push(answer);
+});
+
+
+// app.post('/', function(req, res){
+//   res.sendFile(path.resolve("server/public/views/index.html"));
+// });
+
+
+
 
 app.listen(port, function(){
   console.log("app.js Listening on port: ", port);
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,3 +1,102 @@
 $(document).ready(function(){
   console.log("jQuery sourced from client.js");
+
+
+
+
+
+
+
+
+//unsure whether global variable will work. Maybe belong within on.clicks
+
+
+$(".clear").on("click", function(){
+  console.log("clear click success");
+$(".answerDiv").empty();
+$(".values").empty();
+});//end of clear on click
+ var plusCounter = 0;
+ var minusCounter = 0;
+ var multiplyCounter = 0;
+ var divideCounter = 0;
+ var finalAnswer = {};
+
+$(".operatorPlus").on("click", function(){
+  console.log("operatorPlus click success");
+  plusCounter ++;
+  console.log(plusCounter);
+  // var valueOne = $("#firstValue").val();             //why couldn't I use div classes
+  // var valueTwo = $("#secondValue").val();
+  // console.log(valueOne + valueTwo);
+  // return firstValue + secondValue
+});//end operatorPlus
+
+$(".operatorMinus").on("click", function(){
+  console.log("operatorMinus click success");
+  minusCounter ++;
+  console.log(minusCounter);
+});//end operatorMinus
+
+$(".operatorMultiply").on("click", function(){
+  console.log("operatorMultiply click success");
+  multiplyCounter ++;
+  console.log(multiplyCounter);
+});//end operatorMultiply
+
+$(".operatorDivide").on("click", function(){
+  console.log("operatorDivide click success");
+  divideCounter++;
+  console.log(divideCounter);
+});//end operatorDivide
+
+$(".operatorEquals").on("click", function(){
+  console.log("equal click success");
+var equation = {};
+  var operator = 0;
+                        // switch (equation.operator) {
+                        //   case plus:
+                        //   if(plusCounter == 1) {
+                        //     operator = "plus";
+                        //   }
+                        //     break;
+                        //   case minus:
+                        //   if(minusCounter == 1) {
+                        //     operator = "minus";
+                        //   }
+                        //     break;
+                        //   case multiply:
+                        //   if(multiplyCounter == 1) {
+                        //     operator = "multiply";
+                        //   }
+                        //     break;
+                        //   case divide:
+                        //   if(minusCounter == 1) {
+                        //     operator = "divide";
+                        //   }
+                        //     break;
+                        //
+                        // }
+
+console.log(operator);
+console.log(finalAnswer);
+  equation.firstValue = $("#firstValue").val();
+  equation.secondValue = $("#secondValue").val();
+  equation.operator = operator;
+  console.log(firstValue, secondValue);
+  console.log(equation);
+  $.ajax({
+    type: "POST",
+    url: "/values",
+    data: equation,
+    success: function(response){
+      console.log("client murk");
+      // console.log(response);
+    }
+  });
+});//end equalsOnClick
+
+
+
+
 });//end document ready
