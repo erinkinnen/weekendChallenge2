f71a61f8940404835ec0c6225e2a2ac3da296fd6
Erin Kinnen <erin.kinnen@outlook.com>
2017-03-19 13:11:16 -0500

    adds all but clear

--- server/app.js
+++ server/app.js
@@ -10,9 +10,42 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.post("/values", function(req, res){
   var values= req.body;
   console.log(values);
+  console.log(values.operator);
   // var answer = {};
   // answer.result = (parseInt(values.firstValue) + parseInt(values.secondValue));
-  var answer = (parseInt(values.firstValue) + parseInt(values.secondValue));
+  //*******var answer = (parseInt(values.firstValue) + parseInt(values.secondValue));
+  var operator = values.operator;
+  console.log(operator);
+  var math = function(){
+    switch(operator){
+    case "plus":
+    return (parseInt(values.firstValue) + parseInt(values.secondValue));
+    break;
+    case "minus":
+    return (parseInt(values.firstValue) - parseInt(values.secondValue));
+    break;
+    case "multiply":
+    return (parseInt(values.firstValue) * parseInt(values.secondValue));
+    break;
+    case "divide":
+    return (parseInt(values.firstValue) / parseInt(values.secondValue));
+    default:
+    "not working";
+  }
+};
+var answer = math();
+          //   var math = function(operator) {
+          //     if (operator = plus) {
+          //     return (parseInt(values.firstValue) + parseInt(values.secondValue));
+          //   } else if (operator = "minus") {
+          //     return (parseInt(values.firstValue) - parseInt(values.secondValue));
+          //   } else if (operator = "multiply") {
+          //     return (parseInt(values.firstValue) * parseInt(values.secondValue));
+          //   } else  if (operator = "divide") {
+          //     return (parseInt(values.firstValue) / parseInt(values.secondValue));
+          //   }
+          // };
+          //   var answer = math();
   console.log(answer);
   var finalResult = answer.toString();
   console.log(finalResult);
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,35 +2,19 @@ $(document).ready(function(){
   console.log("jQuery sourced from client.js");
 
 
-
-
-
-
-
-
-//unsure whether global variable will work. Maybe belong within on.clicks
-
-
 $(".clear").on("click", function(){
   console.log("clear click success");
-$(".answerDiv").empty();
+$(".answer").empty();
 $(".values").empty();
 });//end of clear on click
- var plusCounter = 0;
- var minusCounter = 0;
- var multiplyCounter = 0;
- var divideCounter = 0;
+
  var equation = {};
- var finalAnswer = {};
+ // var finalAnswer = {};
 
 $(".operatorPlus").on("click", function(){
   console.log("operatorPlus click success");
   equation.operator = "plus";
   console.log();
-  // var valueOne = $("#firstValue").val();             //why couldn't I use div classes
-  // var valueTwo = $("#secondValue").val();
-  // console.log(valueOne + valueTwo);
-  // return firstValue + secondValue
 });//end operatorPlus
 
 $(".operatorMinus").on("click", function(){
@@ -51,12 +35,10 @@ $(".operatorDivide").on("click", function(){
 $(".operatorEquals").on("click", function(){
   console.log("equal click success");
 
-// console.log(operator);
-console.log(finalAnswer);
+  // console.log(finalAnswer);
   equation.firstValue = $("#firstValue").val();
   equation.secondValue = $("#secondValue").val();
-  // equation.operator = operator;
-  console.log(firstValue, secondValue);
+  // console.log(firstValue, secondValue);
   console.log(equation);
   $.ajax({
     type: "POST",
